95908dd4b69ffbcfb1896c1474b84f8c7a3effc9
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-07-20 13:26:04 -0400

    Create .js

---
+++ .js
@@ -0,0 +1,12 @@
+// Fizzbuzz me, bro
+for (var a = 1; a <= 100; a++) {
+	if (a % 15 === 0) {
+		console.log('FizzBuzz');
+	} else if (a % 3 === 0) {
+		console.log('Fizz');
+	} else if (a % 5 === 0) {
+		console.log('Buzz');
+	} else {
+		console.log(i);
+	}
+}
